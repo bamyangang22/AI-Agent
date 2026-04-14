1a77ddd36939f1ed1da2a4e453b935473881f4ca
henry <wi990415@gmail.com>
2026-04-15 04:10:18 +0900

    FEAT: agent를 콘솔창이 아닌 GUI로 만들기

--- main.py
+++ main.py
@@ -1,14 +1,69 @@
+import dotenv
+
+dotenv.load_dotenv()
+import asyncio
 import streamlit as st
+from agents import Agent, Runner, SQLiteSession
 
-if "is_admin" not in st.session_state:
-    st.session_state["is_admin"] = False
+if "agent" not in st.session_state:
+    st.session_state["agent"] = Agent(
+        name="ChatGPT Clone",
+        instructions="""
+        You are a helpful assistant.
+        """,
+    )
+agent = st.session_state["agent"]
 
-st.header("Hello!")
+if "session" not in st.session_state:
+    st.session_state["session"] = SQLiteSession(
+        "chat-history",
+        "chat-gpt-clone-memory.db",
+    )
+session = st.session_state["session"]
 
-name = st.text_input("What is your name?")
 
-if name:
-    st.write(f"Hello {name}")
-    st.session_state["is_admin"] = True
+async def paint_history():
+    messages = await session.get_items()
 
-print(st.session_state["is_admin"])
+    for message in messages:
+        with st.chat_message(message["role"]):
+            if message["role"] == "user":
+                st.write(message["content"])
+            else:
+                if message["type"] == "message":
+                    st.write(message["content"][0]["text"])
+
+
+asyncio.run(paint_history())
+
+
+async def run_agent(message):
+    with st.chat_message("ai"):
+        text_placeholder = st.empty()
+        response = ""
+        stream = Runner.run_streamed(
+            agent,
+            message,
+            session=session,
+        )
+
+        async for event in stream.stream_events():
+            if event.type == "raw_response_event":
+                if event.data.type == "response.output_text.delta":
+                    response += event.data.delta
+                    text_placeholder.write(response)
+
+
+prompt = st.chat_input("Write a message for your assistant")
+
+if prompt:
+    with st.chat_message("human"):
+        st.write(prompt)
+    asyncio.run(run_agent(prompt))
+
+
+with st.sidebar:
+    reset = st.button("Reset memory")
+    if reset:
+        asyncio.run(session.clear_session())
+    st.write(asyncio.run(session.get_items()))
